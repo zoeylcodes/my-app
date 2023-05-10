372afca71e23d0c2ce96ed8d9f560f32d88b1e33
Zoey <124487432+zoeylcodes@users.noreply.github.com>
2023-05-09 23:47:30 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather />
+        <Weather defaultCity="Berlin" />
       </header>
+
+      <div className="footer text-center">
+        <a
+          href="https://github.com/zoeylcodes/my-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source
+        </a>{" "}
+        code by Zoey Leyoussi✨
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,127 +1,149 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function Weather() {
+export default function Weather(props) {
   let forecastData = [
     {
       day: "Monday",
       description: "Showers",
       imgUrl: "https://ssl.gstatic.com/onebox/weather/64/rain.png",
+      forecasted: "10|5",
     },
     {
       day: "Tuesday",
       description: "Sunny",
       imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      forecasted: "16|7",
     },
     {
       day: "Wednesday",
       description: "Cloudy",
       imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+      forecasted: "14|5",
     },
     {
       day: "Thursday",
       description: "Thunderstorm",
       imgUrl: "https://ssl.gstatic.com/onebox/weather/64/thunderstorms.png",
+      forecasted: "11|13",
     },
     {
       day: "Friday",
       description: "Snow",
       imgUrl: "https://ssl.gstatic.com/onebox/weather/64/snow.png",
+      forecasted: "20|10",
     },
   ];
 
-  return (
-    <div className="Weather">
-      <form className="mb-3">
-        <div className="row mx-auto">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city..."
-              className="form-control w-90"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-sm-3 d-flex justify-content-center justify-content-sm-end mt-2 mt-sm-0 mx-auto">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100 me-2 mb-2 mb-sm-0"
-            />
-            <input
-              type="submit"
-              value="Current"
-              className="btn btn-primary w-100 me-2 mb-2 mb-sm-0"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="overview">
-        <h1>Toronto</h1>
-        <ul className="float-left">
-          <li>Last updated: Sunday 10:00</li>
-          <li>Cloudy</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              alt="Sunny"
-              className="float-left img-fluid mx-auto"
-            />
-            <div className="float-left">
-              <strong>19</strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      date: new Date(response.data.dt * 1000),
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
+      date: "Sunday 10:00",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form className="mb-3">
+          <div className="row mx-auto">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city..."
+                className="form-control w-90"
+                autoComplete="off"
+              />
+            </div>
+            <div className="col-sm-3 d-flex justify-content-center justify-content-sm-end mt-2 mt-sm-0 mx-auto">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100 me-2 mb-2 mb-sm-0"
+              />
+              <input
+                type="submit"
+                value="Current"
+                className="btn btn-primary w-100 me-2 mb-2 mb-sm-0"
+              />
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 80%</li>
-            <li>Wind: 10 km/h</li>
+        </form>
+        <div className="overview">
+          <h1>{weatherData.city}</h1>
+          <ul className="float-left">
+            <li>
+              Last updated: {weatherData.date} {weatherData.time}
+            </li>
+            <li className="text-capitalize">{weatherData.description}</li>
           </ul>
         </div>
-      </div>
-
-      <br />
-
-      <div className="Forecast">
-        <div className="row justify-content-center">
-          {forecastData.map((day) => (
-            <div className="col" key={day.day}>
-              <div className="card h-100 " style={{ width: "10rem" }}>
-                <img
-                  src={day.imgUrl}
-                  alt={day.description}
-                  className="img-fluid mx-auto text-center"
-                />
-                <div className="card-body text-center ">
-                  <h5 className="card-title text-center">{day.day}</h5>
-                  <p className="card-text text-center">{day.description}</p>
-                </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="float-left img-fluid mx-auto"
+              />
+              <div className="float-left">
+                <strong>{Math.round(weatherData.temperature)}</strong>
+                <span className="units">
+                  <a href="/">°C</a> | <a href="/">°F</a>
+                </span>
               </div>
             </div>
-          ))}
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {Math.round(weatherData.humidity)} %</li>
+              <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <br />
 
-      <div className="footer text-center">
-        <a
-          href="https://github.com/zoeylcodes/my-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open source
-        </a>{" "}
-        code by Zoey Leyoussi✨
+        <br />
+
+        <div className="Forecast">
+          <div className="row justify-content-center text-center">
+            {forecastData.map((day) => (
+              <div className="col" key={day.day}>
+                <div className="card h-100 " style={{ width: "10rem" }}>
+                  <img
+                    src={day.imgUrl}
+                    alt={day.description}
+                    className="img-fluid mx-auto text-center"
+                  />
+                  <div className="card-body text-center ">
+                    <h5 className="card-title text-center">{day.forecasted}</h5>
+                    <h5 className="card-title text-center">{day.day}</h5>
+                    <p className="card-text text-center">{day.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <br />
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "e22ed9083fe0fa0dbcc4f7e384978903";
+    let city = "toronto";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=metric&appid=${apiKey}
+`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
