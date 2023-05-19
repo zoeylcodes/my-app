5e91116361c3f0fbfdb5a8b87cdf51341f23c3d8
Zoey <124487432+zoeylcodes@users.noreply.github.com>
2023-05-19 11:39:41 -0400

    updated

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -11,6 +11,11 @@ const WeatherApp = () => {
       "The only way to do great work is to love what you do. - Steve Jobs",
       "Believe you can and you're halfway there. - Theodore Roosevelt",
       "The best way to predict the future is to create it. - Peter Drucker",
+      "The only limit is your mind. - Unknown",
+      "Dream big and dare to fail.- Norman Vaughan",
+      "Stay hungry, stay foolish.- Steve Jobs",
+      "Action is the foundational key to all success.- Pablo Picasso",
+      "Don't watch the clock; do what it does. Keep going.- Sam Levenson",
     ];
 
     const randomIndex = Math.floor(Math.random() * quotes.length);
